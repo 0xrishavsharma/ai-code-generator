1ec3183b7c1c5b75f9999ba0d17b7afa742f7979
Rishav Sharma <rishavtosharma@gmail.com>
2024-04-06 17:16:44 +0530

    ci: added basic route to check API functionality

--- src/app/[[...chatId]]/chat-content.tsx
+++ src/app/[[...chatId]]/chat-content.tsx
@@ -3,15 +3,22 @@
 import ChatInput from "@/components/chat-input";
 
 export default function ChatContent() {
-  const handleSubmit = (value: string, file?: File) => {
-    console.log("submit", value, file);
-  };
-  return (
-    <>
-      <div className="max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto">
-        AI Content goes here
-      </div>
-      <ChatInput onSubmit={handleSubmit} />
-    </>
-  );
+    const handleSubmit = async (value: string, file?: File) => {
+        const res = fetch("/api/message", {
+            method: "POST",
+            body: JSON.stringify({ content: value }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        console.log("submit", res);
+    };
+    return (
+        <>
+            <div className='max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto'>
+                AI Content goes here
+            </div>
+            <ChatInput onSubmit={handleSubmit} />
+        </>
+    );
 }
