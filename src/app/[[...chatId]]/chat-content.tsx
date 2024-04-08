fe5fefd8658e398df857841c393c512ea4fd2589
Rishav Sharma <rishavtosharma@gmail.com>
2024-04-08 13:50:22 +0530

    ci: added response streaming and styling, added husky and commitlint configs

--- src/app/[[...chatId]]/chat-content.tsx
+++ src/app/[[...chatId]]/chat-content.tsx
@@ -1,24 +1,81 @@
-"use client";
+"use client"
 
-import ChatInput from "@/components/chat-input";
+import ChatInput from "@/components/chat-input"
+import { useState } from "react"
+import Markdown from "react-markdown"
+import remarkGfm from "remark-gfm"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { duotoneDark as dark } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 export default function ChatContent() {
-    const handleSubmit = async (value: string, file?: File) => {
-        const res = fetch("/api/message", {
-            method: "POST",
-            body: JSON.stringify({ content: value }),
-            headers: {
-                "Content-Type": "application/json",
+  const [assistantResponse, setAssistantResponse] = useState("")
+  const handleSubmit = async (value: string, file?: File) => {
+    console.log(
+      value ? "Question asked: " : "",
+      file ? "File Uploaded: " : "",
+      value,
+      file ? file : ""
+    )
+    const res = await fetch("/api/message", {
+      method: "POST",
+      body: JSON.stringify({ content: value }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    if (!res.ok || !res.body) {
+      console.error("Error:", res.status, res.statusText, await res.text())
+      return
+    }
+    const reader = res.body.getReader()
+
+    // created this loop to read the stream until we get "true" in the done property
+    const decoder = new TextDecoder()
+    let finalText = ""
+
+    while (true) {
+      const { done, value } = await reader.read()
+
+      // Since the value we'll get while reading the steam is in binary, we need to convert it into text
+      const decodedText = decoder.decode(value, { stream: !done })
+      setAssistantResponse((prevStreamedRes) => prevStreamedRes + decodedText)
+
+      if (done) {
+        break
+      }
+    }
+  }
+  return (
+    <>
+      <div className="max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto prose dark:prose-invert">
+        <Markdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code(props) {
+              const { children, className, node, ...rest } = props
+              const match = /language-(\w+)/.exec(className || "")
+              return match ? (
+                <SyntaxHighlighter
+                  PreTag="div"
+                  language={match[1]}
+                  style={dark}
+                  wrapLines
+                  wrapLongLines
+                >
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                <code {...rest} className={className}>
+                  {children}
+                </code>
+              )
             },
-        });
-        console.log("submit", res);
-    };
-    return (
-        <>
-            <div className='max-w-4xl w-full mx-auto flex-1 px-10 py-5 overflow-x-hidden overflow-y-auto'>
-                AI Content goes here
-            </div>
-            <ChatInput onSubmit={handleSubmit} />
-        </>
-    );
+          }}
+        >
+          {assistantResponse}
+        </Markdown>
+      </div>
+      <ChatInput onSubmit={handleSubmit} />
+    </>
+  )
 }
--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,19 +1,21 @@
-import OpenAI from "openai";
+import OpenAI from "openai"
+import { OpenAIStream, StreamingTextResponse } from "ai"
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+  apiKey: process.env.OPENAI_API_KEY,
+})
 export async function POST(req: Request) {
-    const { content } = await req.json();
+  const { content } = await req.json()
 
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content }],
-        model: "gpt-4-turbo-preview",
-        stream: true,
-    });
+  // Take the content and send it to OPENAI's GTP4 Vision model
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content }],
+    model: "gpt-4-turbo-preview",
+    stream: true,
+  })
 
-    // Take the content and send it to OPENAI's GTP4 Vision
+  // Returning the response as a steam to the frontend/client
+  const stream = OpenAIStream(chatCompletion)
 
-    console.log("Content on server", { content });
-    return new Response("Alright!");
+  return new StreamingTextResponse(stream)
 }
