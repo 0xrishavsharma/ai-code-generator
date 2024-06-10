ca0644308ca35c10dd293fe02c33913e2e58a06c
Rishav Sharma <rishavtosharma@gmail.com>
2024-06-10 08:15:41 +0530

    ci(auth): Initialise auth js

--- src/app/actions/GetSignedUrl.ts
+++ src/app/actions/GetSignedUrl.ts
@@ -1,5 +1,6 @@
 "use server"
-// This function is getting created to generate a url that a client can use to send the file to the server. If we don't have this server function, the client can upload or delete whatever they want in the bucket
+
+// This function is used to generate a url that a client can use to send the file to the S3 bucket and if we don't have this server function, acting as an intermediary, the user can upload or delete whatever they want in the bucket
 export async function getSignedUrl() {
   return { success: { url: "" } }
 }
